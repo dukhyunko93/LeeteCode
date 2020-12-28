7321b8d63423f4ed632d4c32e4d7694b99425ac5
dukhyunko93 <dukhyunko93@gmail.com>
2020-12-28 18:14:24 -0500

    longest palindrom subtring

--- longest-palindromic-substring.js
+++ longest-palindromic-substring.js
@@ -11,18 +11,42 @@
 // Output: "bb"
 
 var longestPalindrome = function(s) {
-    let i = 0
-    let j = s.length - 1;
-    let max = ""
-    while(i < s.length - 1 && j >= i){
-        if(s[i] === s[j]){
-            i++;
-        }
-        j--;
-    }
-    console.log(i)
-};
+	if(s.length <= 1) return s;
+	
+    const dp = [...new Array(s.length + 1)].map(_ => new Array(s.length + 1).fill(false));
+    let lps = '';
 
-let s = "aaaabbbcd"
+    for(let i = 0; i < s.length; i++) {
+        dp[i][i] = true;
+        lps = s[i];
+    }
+    
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === s[i + 1]) dp[i][i+1] = true;
+        if(dp[i][i+1]) lps = s.substring(i, i + 2);
+    }
+
+    let j;
+    for(let k = 2; k < s.length; k++){
+        for(let i = 0; i < s.length; i ++){
+            j = i + k;
+            dp[i][j] = dp[i+1][j-1] && s[i] === s[j];
+            if(dp[i][j]) lps = lps.length < (j - i + 1) ? s.substring(i, j + 1) : lps;
+        }
+    }
+    // for(let i = 0; i < s.length; i++) {    
+    //     for(let j = i + 2; j < s.length; j++) {
+    //         dp[i][j] = dp[i+1][j-1] && s[i] === s[j];
+    //         if(dp[i][j]) lps = lps.length < (j - i + 1) ? s.substring(i, j + 1) : lps;
+    //     }
+    // }
+    
+    return console.log(lps);
+}
+
+let s = "cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj"
 
 longestPalindrome(s)
+
+
+// node longest-palindromic-substring
