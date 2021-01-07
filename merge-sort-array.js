b95626ce211dd8180dd41bf09fdff04bc982d0ea
dukhyunko93 <dukhyunko93@gmail.com>
2021-01-07 18:23:39 -0500

    merge sort array

---
+++ merge-sort-array.js
@@ -0,0 +1,9 @@
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m, n, ...nums2)
+    nums1.sort((a, b) => a - b)
+};
+
+let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3;
+merge(nums1, m, nums2, n)
+// node merge-sort-array.js
+
